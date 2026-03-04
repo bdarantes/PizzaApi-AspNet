adf91f27a3763aa3d323f71084784f96f20b8410
BrazD <brazdanarantes@gmail.com>
2026-03-04 10:00:18 -0300

    chore: fix dotnet-ef migration command syntax

---
+++ Migrations/20260304125944_AdicionandoProdutoAoPedido.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzariaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoProdutoAoPedido : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProdutoId",
+                table: "Pedidos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_ProdutoId",
+                table: "Pedidos",
+                column: "ProdutoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedidos_Produtos_ProdutoId",
+                table: "Pedidos",
+                column: "ProdutoId",
+                principalTable: "Produtos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedidos_Produtos_ProdutoId",
+                table: "Pedidos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pedidos_ProdutoId",
+                table: "Pedidos");
+
+            migrationBuilder.DropColumn(
+                name: "ProdutoId",
+                table: "Pedidos");
+        }
+    }
+}
